af715d8b9f7bc0ee3fec5e0d13e82f66c585a04a
Anthony <famedanthony@hotmail.com>
2016-09-29 15:43:19 -0400

    8XY5

--- chip8.c
+++ chip8.c
@@ -14,7 +14,7 @@ typedef struct chip8
 	unsigned char memory[4096];
 	unsigned char V[16];            // 15 8-bit registers
 	unsigned char key[16];
-	unsigned char display[64 * 32]; // 2048 pixel graphics
+	//unsigned char display[64 * 32]; // 2048 pixel graphics
 	unsigned short stack[16];
 	int drawFlag;
 	unsigned short sp;              // Stack pointer
@@ -42,9 +42,9 @@ void setup_graphics()
 	else
 	{
 		// Just here to make sure the window works
-		SDL_Delay(3000);
-		SDL_DestroyWindow(window);
-		SDL_Quit();
+		//SDL_Delay(3000);
+		//SDL_DestroyWindow(window);
+		//SDL_Quit();
 	}
 }
 
@@ -58,8 +58,13 @@ void setup_input()
 void chip8_init(C8 *CH8)
 {
 	char game_name[100];
-	int i;
+	//int i = 0;
 	
+	CH8->i      = 0;     // Reset index register
+	CH8->sp     = 0;     // Reset stack pointer
+	CH8->opcode = 0;     // Reset current opcode
+	CH8->pc     = 0x200; // Program counter starts at 0x200
+
 	printf("Enter name of game: ");
 	scanf("%s", game_name);
 
@@ -73,20 +78,18 @@ void chip8_init(C8 *CH8)
 	}
 
 	fread(CH8->memory+0x200, 1, memsize-0x200, CH8->game); // load game into memory
-/*
-	for (i = 0; i < 80; ++i)
-		CH->memory[i] = chip8_fontset[i];                  // load fontset into memory
-*/
 	memset(CH8->display, 0, sizeof(CH8->display));         // clear graphics
 	memset(CH8->stack, 0, sizeof(CH8->stack));             // clear stack
-	memset(CH8->V, 0, sizeof(CH8->V));                     // clear chip8 register
+	memset(CH8->V, 0, sizeof(CH8->V));                     // clear registers
 
-	CH8->sp &= 0;
-	CH8->pc = 0x200;
-	CH8->opcode = 0x200;
+	// Load fontset
+	/*
+	for (int i = 0; i < 80; ++i)
+		CH8->memory[i] = chip8_fontset[i];	
+		*/
 }
 
-void timers(C8 * CH8)
+void chip8_timers(C8 *CH8)
 {
 	if (CH8->delay_timer > 0)
 		CH8->delay_timer--;
@@ -98,14 +101,11 @@ void timers(C8 * CH8)
 
 void emulate_cycle(C8 *CH8)
 {
-	Uint8 * keys;
-	int y, x, vx, vy, times, i;
-	unsigned height, pixel;
+	CH8->opcode = CH8->memory[CH8->pc] << 8 | CH8->memory[CH8->pc + 1]; // Merge both bytes for opcode
 
-
-	switch (CH8->opcode & 0xF000)
+	switch (CH8->opcode & 0xF000) // Decode opcode
 	{
-		case 0x000:
+		case 0x0000:
 			switch (CH8->opcode & 0x000F)
 			{
 				case 0x0000:                                   // 00E0: CLS - Clear the display
@@ -164,7 +164,80 @@ void emulate_cycle(C8 *CH8)
 			CH8->V[(CH8->opcode & 0x0F000) >> 8] += (CH8->opcode & 0x00FF);
 			CH8->pc += 2;
 			break;
+		case 0x8000:
+			switch (CH8->opcode & 0x000F)
+			{
+				case 0x0000: // 8XY0: Sets VX to the value of VY
+					CH8->V[(CH8->opcode & 0x0F00) >> 8] = CH8->V[(CH8->opcode & 0x00F0) >> 4];
+					CH8->pc += 2;
+					break;
+
+				case 0x0001: // 8XY1: Sets VX to VX or VY
+					CH8->[(CH8->opcode & 0x0F00) >> 8] = CH8->V[(CH8->opcode & 0x0F00) >> 8] | CH8->V[(CH8->opcode & 0x00F0) >> 4];
+					CH8->pc += 2;
+					break;
+
+				case 0x0002: // 8XY2: Sets VX to VX and VY
+					CH8->[(CH8->opcode & 0x0F00) >> 8] = CH8->V[(CH8->opcode & 0x0F00) >> 8] & CH8->V[(CH8->opcode & 0x00F0) >> 4];
+					CH8->pc += 2;
+					break;
+
+				case 0x0003: // 8XY3: Sets VX to VX xor VY
+					CH8->[(CH8->opcode & 0x0F00) >> 8] = CH8->V[(CH8->opcode & 0x0F00) >> 8] ^ CH8->V[(CH8->opcode & 0x00F0) >> 4];
+					CH8->pc += 2;
+					break;
+
+				case 0x0004: // 8XY4: Adds VY to VX. VF is set to 1 when there's a carry, and to 0 when there's not.
+					if (CH8->V[(CH8->opcode & 0x00F0) >> 4] > (0xFF - CH8->V[(CH8->opcode & 0x0F00) >> 8]))
+						CH8->V[0xF] = 1; // carry
+					else
+						CH8->V[0xF] = 0;
+					CH8->V[(CH8->opcode & 0x0F00) >> 8] += CH8->V[(CH8->opcode & 0x00F0) >> 4];
+					CH8->pc += 2;
+					break;
+
+				case 0x005: // 8XY5: Set Vx = Vx - Vy. If VX > VY then VF = 1, otherwise it's 0
+					if (((int)CH8->V[(CH8->opcode & 0x0F00) >> 8] - (int)CH8->V[(CH8->opcode & 0x00F0) >> 4]) >= 0)
+						CH8->V[0xF] = 1;
+					else
+						CH8->V[0xF] &= 0;
+					CH8->V[(CH8->opcode & 0x0F00) >> 8] -= CH8->V[(CH8->opcode & 0x00F0) >> 4];
+					CH8->pc += 2;
+						
+			}
+			break;
+
+		case 0xD000: // DXYN: Responsible for drawing to the display 
+			// Get the position and height of the sprite
+			unsigned short x = CH8->V[(CH8->opcode & 0x0F00) >> 8]; 
+			unsigned short y = ch8->V[(opcode & 0X00F0) >> 4];
+			unsigned short height = CH8->opcode & 0x000F;      // Pixel value
+			unsigned short pixel;
+			V[0xF] = 0;                                        // Reset register VF
+			for (int yline = 0; yline < height; yline++)       // loop over each row
+			{
+				pixel = CH8->memory[CH8->i + yline];           // fetch pixel value from memory starting at I
+				for (int xline = 0; xline < 8; xline++)        // loop over 8 bits of one row
+				{
+					if ((pixel & (0x80 >> xline)) != 0)        // Check if current evaluated pixel is 1. (0x80 >> xline scan through the byte, one bit at a time)
+					{
+						// Check if the pixel on display is set to 1, if so, register
+						// collision by setting VF register
+						if (CH8->display[(x + xline + ((y + yline) * 64))] == 1)
+							CH8->V[0xF] = 1;
+						CH8->display[x + xline + ((y + yline) * 64)] ^= 1; // Set pixel value 
+					}
+				}
+			}
+			CH8->drawFlag = 1; 
+			CH8->pc += 2;
+			break;
+
+		default:
+			printf("Unknown opcode: 0x%X\n", CH8->opcode);
 	}
+
+	chip8_timers(CH8); // Update timers
 }
 
 int main(int argc, char **argv)
@@ -173,11 +246,11 @@ int main(int argc, char **argv)
 	
 	setup_graphics();
 	setup_input();
-	chip8_init(&CH8);
+	chip8_init(CH8);
 	
 	for (;;)
 	{
-		emulate_cycle(&CH8); // emulate one cycle
+		emulate_cycle(CH8);
 	}
 	
 	return 0;
